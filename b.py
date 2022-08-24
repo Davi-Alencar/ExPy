ddfac5df24c8faf44ac4504e51288e70a784dc23
Davi <davialencar101@gmail.com>
2022-08-24 11:57:51 -0300

    24/08

---
+++ b.py
@@ -0,0 +1,4 @@
+a = int(input("Digite número 1: "))
+b = int(input("Digite número 2: "))
+soma = a + b
+print (soma)
---
+++ c.py
@@ -0,0 +1,4 @@
+raio = float(input("Digite o raio do tonel: "))
+altura = float(input("Digite a altura do tonel: "))
+volume = 3.1415 * (raio**) * altura
+print(volume)
