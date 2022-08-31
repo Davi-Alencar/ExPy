7ebdd3c8fc75c231d862707f9c3c22e239913981
Davi <davialencar101@gmail.com>
2022-08-31 10:20:55 -0300

    31/08

--- d.py
+++ d.py
@@ -1,7 +1,6 @@
-a = int(input("Digite número a"))
-b = int(input("Digite número b"))
-int c 
-c <- a
-a <- b
-b <- c
+a = int(input("Digite número a: "))
+b = int(input("Digite número b: ")) 
+c = a
+a = b
+b = c
 print (a, b)
---
+++ e.py
@@ -0,0 +1,8 @@
+#Solicite o comprimento, largura, e altura de um retângulo
+#Apresente volume com: V= c*l*a
+
+comprimento = float(input("Digite Comprimento do Retângulo: "))
+largura = float(input("Digite Largura do Retângulo: "))
+altura = float(input("Digite Altura do Retângulo: "))
+volume = comprimento * largura * altura
+print(volume)
